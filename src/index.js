d974f093ec44aa00608f712a91fbcccbb2bea295
NicolasVivalda <nicovivalda@gmail.com>
2022-12-03 11:28:37 -0300

    fix github pages

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { App } from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename= "/pokedex-react">
+    <BrowserRouter>
     <App />
     </BrowserRouter>
   </React.StrictMode>
